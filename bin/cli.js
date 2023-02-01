4ef98accf2fe2660b591595eda6a425c8e56ddde
SyimykMitalipove <syimyk310703@gmail.com>
2023-02-01 19:27:15 +0600

    update

--- bin/cli.js
+++ bin/cli.js
@@ -1,24 +1,30 @@
 #!usr/bin/env node
-const childProcess = require('child_process');
+const proChild = require('child_process');
+
 const runCommand = command => {
-    try {
-        childProcess.execSync(`${command}`, { stdio: 'inherit' })
-    } catch (error) {
-        console.error(`Failed to execute ${command}`, error);
-        return false;
-    }
-    return true;
-}
-const repoName = process.argv[2];
+  try {
+    proChild.execSync(`${command}, { stdio: 'inherit' }`);
+  } catch (error) {
+    console.error(`Failed to execute ${command}`, error);
+    return false;
+  }
+  return true;
+};
+
+const repoName = process.argv[ 2 ];
 const gitCheckoutCommand = `git clone --depth 1 https://github.com/SyimykMitalipov/create-webpack-syimyk ${repoName}`;
 const installDepsCommand = `cd ${repoName} && npm install`;
-console.log(`Cloning the repo from ${repoName}`);
-const checkout = runCommand(gitCheckoutCommand);
-if (!checkout) process.exit(-1);
+
+console.log(`Cloning the repository with name ${repoName}`);
+const checkedOut = runCommand(gitCheckoutCommand);
+
+if (!checkedOut) process.exit(-1);
+
 console.log(`Installing dependencies for ${repoName}`);
+
 const installDeps = runCommand(installDepsCommand);
+
 if (!installDeps) process.exit(-1);
 
-
-console.log(`Congratulations! You are ready`);
-console.log(`cd ${repoName} && npm start`);
+console.log('Congratulations! You are ready. Follow the followint commands to start');
+console.log(`cd ${repoName} && npm start`)
